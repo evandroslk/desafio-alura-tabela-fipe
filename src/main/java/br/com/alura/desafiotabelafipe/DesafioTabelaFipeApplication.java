ea35f38246e64d43d0774f7eabbe30db556f59c5
Evandro Lovato <evandroslk@gmail.com>
2024-05-20 14:04:50 -0300

    Criação do Projeto

---
+++ src/main/java/br/com/alura/desafiotabelafipe/DesafioTabelaFipeApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.desafiotabelafipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioTabelaFipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioTabelaFipeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/alura/desafiotabelafipe/DesafioTabelaFipeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.desafiotabelafipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioTabelaFipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
